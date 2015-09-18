e5bd9ee0fb5fdadb6f27e5d27c746811b95b5519
Dirk Volland <dv.junin@gmail.com>
2015-09-18 11:27:56 +0200

    first commit

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  id: 'com.fileReaderTest', 
+  name: 'fileReaderTest',
+  description: '',
+  author: '',
+  email: '',
+  website: '',
+  version: '0.0.1',
+  buildNumber: '105'
+});
+
+App.accessRule("*");//should be a little more tightly controlled
